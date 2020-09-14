a95bbe39e42e8bd0ffb8a578801ca0ebd7404069
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-09-15 01:48:16 +0530

    Modified path again

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ project = trainer.create_project("My New Project")
 hemlock_tag = trainer.create_tag(project.id, "Hemlock")
 cherry_tag = trainer.create_tag(project.id, "Japanese Cherry")
 
-base_image_url = "https://github.com/" + os.environ["GITHUB_REPOSITORY"] +"/tree/master/"
-print (base_image_url)
+base_image_url = "https://github.com/" + os.environ["GITHUB_REPOSITORY"] +"/blob/master/"
 
 print("Adding images...")
 
